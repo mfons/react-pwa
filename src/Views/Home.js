a1eb4c37b27c5a49092ec3493763d3123e532728
Fons <Michael.Fons@travelport.com>
2020-11-24 17:34:43 -0700

    first commit to fons-work

---
+++ src/Views/Home.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import Loader from '../Components/Loader'
+import ProductCard from '../Components/ProductCard'
+import { useAxiosGet } from '../Hooks/HttpRequests'
+
+function Home() {
+    const url = `https://5f15e386a346a0001673894a.mockapi.io/products?page=1&limit=10`
+
+    let products = useAxiosGet(url);
+    let content = null;
+
+    if (products.error /*|| products.data && products.data.images.length === 0*/) {
+        content = <p>
+            Omg!  Error time!!  Or lack of data or something!!  Better panic and run in little tiny circles...
+        </p>
+    }
+
+    if (products.loading) {
+        content = <Loader></Loader>
+    }
+
+    if(products.data /*&& products.data.images.length > 0*/) {
+        content =
+            products.data.map((product, key) => 
+                <div key={key}>
+                    <ProductCard
+                        product={product}
+                    />
+                </div>
+            )
+        ;
+    }
+
+    return (
+        <div>
+            <h1 className="font-bold text-2xl">
+                Best Sellers
+            </h1>
+            {content}
+        </div>
+    )
+}
+
+export default Home
---
+++ src/Views/Product.js
@@ -0,0 +1,55 @@
+import React from 'react'
+
+import { useParams } from 'react-router-dom'
+import Loader from '../Components/Loader'
+import { useAxiosGet } from '../Hooks/HttpRequests'
+
+function Product() {
+    const { id } = useParams()
+    const url = `https://5f15e386a346a0001673894a.mockapi.io/products/${id}`
+    //const url = `https://5f15e386a346a0001673894a.mockapi.io/products?page=1&limit=10`
+
+    let product = useAxiosGet(url);
+
+    let content = null;
+
+    if (product.error || product.data && product.data.images.length === 0) {
+        content = <p>
+            Omg!  Error time!!  Or lack of data or something!!  Better panic and run in little tiny circles...
+        </p>
+    }
+
+    if (product.loading) {
+        content = <Loader></Loader>
+    }
+
+    if(product.data && product.data.images.length > 0) {
+        content =
+            <div>
+                <h1 className="text-2xl font-bold mb-3">
+                    {product.data.name}
+                </h1>
+                <div>
+                    <img
+                        src={product.data.images[0].imageUrl}
+                        alt={product.data.name}
+                    />
+                </div>
+                <div className="text-xl font-bold mb-3">
+                    {product.data.price}
+                </div>
+                <div >
+                    {product.data.description}
+                </div>
+            </div>
+        ;
+    }
+
+    return(
+        <div>
+            {content}
+        </div>
+    );
+}
+
+export default Product
